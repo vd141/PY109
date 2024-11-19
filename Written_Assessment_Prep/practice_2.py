bfb240a459250c3923abcb2cc7711b30fa94ffa0
Victor Dinh <vd141@users.noreply.github.com>
2024-11-19 23:55:19 +0000

    completed first attempt explanation

--- Written_Assessment_Prep/practice_2.py
+++ Written_Assessment_Prep/practice_2.py
@@ -8,3 +8,9 @@ def my_func():
     print(hello)
 
 my_func()
+
+''' First attempt - 00:01:15
+The variable hello is assigned to the value of 'Hello, world!'. It exists in the global scope
+A function is designed to print the value of hello from the global scope
+the function is invoked, printing the value of hello: 'Hello, world!'
+'''

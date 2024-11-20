32de6eb5ac585a3aa3ca05412929dcfc26ebbed6
Victor Dinh <vd141@users.noreply.github.com>
2024-11-20 00:12:19 +0000

    first attempt completed. Learned how to explain a function whose return value was not assigned to a variable

---
+++ Written_Assessment_Prep/practice_1.py
@@ -0,0 +1,24 @@
+'''
+Practice problem 1: What does this code do and why?
+'''
+
+greeting = 'Hello'
+
+def greet():
+    greeting = 'Hi'
+    return greeting
+
+greet()
+print(greeting)
+
+
+''' Attempt 1: 00:02:40 - Learned how to explain a variable that is not assigned to a function's output/return value
+The variable greeting is assigned to hte value of 'Hello' in the global scope
+a Function called greet sets a local variable named greeting to the value of 'Hi'
+the pointer to this value is returned
+
+the function is invoked, but a variable is not assigned to the output of the function (aka its return value).
+The function invocation effectively does nothing to affect the value of the gloal greeting variable
+
+the variable greeting from the global scope that was assigned to the value of 'Hello' on line 5 is printed
+'''

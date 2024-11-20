5146392e9009c6f437c5a6ca42a396801a3ea3fd
vd141 <victor.dinh141@gmail.com>
2024-11-19 18:44:28 -0800

    completed problem 2)

--- Written_Assessment_Prep/Python_SPOT_Wiki/type_coercions_explicit_and_implicit.py
+++ Written_Assessment_Prep/Python_SPOT_Wiki/type_coercions_explicit_and_implicit.py
@@ -14,3 +14,21 @@ the same type.
 In this example, y is coerced from an integer to a float. This is done
 implicitly.
 '''
+################################################################################
+
+'''
+What coercion is happening here? Is it implicit or explicit?
+'''
+
+a = 1
+b = 2
+print(a + b)
+
+'''
+This is an example of an int being coerced into a string. a and b are both 
+integers, so the result of the operation in the parentheses is an int. The print
+function prints out strings only, so the result of the operation, 3, is coerced
+into a string
+
+This is another example of an implicit coercion
+'''

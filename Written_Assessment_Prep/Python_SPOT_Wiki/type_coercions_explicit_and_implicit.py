7ea455735d7ef791027f63063182cad80562a06e
vd141 <victor.dinh141@gmail.com>
2024-11-19 21:09:06 -0800

    completed first pass of type coercion problems

--- Written_Assessment_Prep/Python_SPOT_Wiki/type_coercions_explicit_and_implicit.py
+++ Written_Assessment_Prep/Python_SPOT_Wiki/type_coercions_explicit_and_implicit.py
@@ -32,3 +32,23 @@ into a string
 
 This is another example of an implicit coercion
 '''
+################################################################################
+
+'''
+What coercion is happening here? Is it implicit or explicit?
+'''
+
+month = "December"
+day = int(input("What day is it? "))
+print(f"Today is the {day} of {month}")
+
+'''
+In the line where day is assigned to the value of the right side of the 
+assignment operator, this is an example of an explicit coercion. The input()
+function returns a string value, which is coerced into an integer by the int() 
+function.
+
+On the print() line, this is an example of implicit coercion. Day is an integer 
+that is coerced into a string in order to be printed by the print function. 
+Month is already a string, so it is not coerced.
+'''

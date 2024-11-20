086cf023a9eb10db70e358072a6c3aa22f4c2155
vd141 <victor.dinh141@gmail.com>
2024-11-19 21:08:42 -0800

    initial commit of practice problems for extra review

---
+++ Written_Assessment_Prep/Python_SPOT_Wiki/EXTRA_PRACTICE.py
@@ -0,0 +1,13 @@
+'''
+What is happening on line 7?
+'''
+
+month = "December"
+day = int(input("What day is it? "))
+print(f"Today is the {day} of {month}")
+
+'''
+Line 7 is a call to a print function, which is taking an f-string. An f-string 
+allows string interpolation by coercing the value of the expression/variable in 
+braces into a string
+'''

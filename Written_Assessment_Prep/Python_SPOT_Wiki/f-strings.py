1360b9b30099d197e2f04148349f8bba9694a04a
vd141 <victor.dinh141@gmail.com>
2024-11-23 14:47:14 -0800

    completed f-strings

---
+++ Written_Assessment_Prep/Python_SPOT_Wiki/f-strings.py
@@ -0,0 +1,22 @@
+'''
+What is an f-string?
+
+An f-string is a string with special syntax that allows the user to interpolate
+Python expressions in the string. The f-string is indicated by an f preceding 
+the string. Anything inside the braces in the f-string is interpolated.
+'''
+
+'''
+What does this code print and why? What concept does this demonstrate?
+'''
+
+name = 'Abraham Lincoln'
+print(f'{name} was a President of the US')
+
+'''
+This code prints 'Abraham Lincoln was a President of the US'
+
+This demonstrates the use of an f-string. the value of the variable name is 
+interpolated into the string when it is referenced inside the braces
+'''
+

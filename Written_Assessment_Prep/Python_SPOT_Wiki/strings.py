6d23a461c5e57ef6f084799a59b5cdbf10213226
vd141 <victor.dinh141@gmail.com>
2024-11-23 14:42:02 -0800

    completed strings wiki questions

--- Written_Assessment_Prep/Python_SPOT_Wiki/strings.py
+++ Written_Assessment_Prep/Python_SPOT_Wiki/strings.py
@@ -19,3 +19,26 @@ sub2 is assigned to the string that is the reverse of the characters in str1
 sub3 is assigned to the string that is comprised of every other character in
 str1
 '''
+
+'''
+What does this code do and why? What concept is this?
+'''
+
+print("Hello\nWorld")
+
+'''
+This string uses an escape character, which is the backslash \, combined with
+n to tell python to print everything after \n to print to a new line
+'''
+
+'''
+What does this code do and why? What concept is this?
+'''
+name = 'Alexander Graham Bell'
+print(name[0])
+
+'''
+This code assigns a variable name to the string with value 'Alexander Graham 
+Bell'. The next line prints the first element of that string, which is an 
+example of string indexing. Indexes of strings start at 0 in Python.
+'''

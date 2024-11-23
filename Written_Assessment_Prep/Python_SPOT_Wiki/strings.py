b457c005b373e5930b42629113eb32faaeaedf40
vd141 <victor.dinh141@gmail.com>
2024-11-23 11:35:16 -0800

    completed first question

---
+++ Written_Assessment_Prep/Python_SPOT_Wiki/strings.py
@@ -0,0 +1,21 @@
+'''
+What is the output of this code and why? What is the concept covered here?
+'''
+
+str1 = "Hello, world!"
+sub1 = str1[8:12]
+print(sub1)
+sub2 = str1[::-1]
+print(sub2)
+sub3 = str1[::2]
+print(sub3)
+
+'''
+sub1 is assigned to the string that is made from the elements ranging from index 
+8 to 12 in str1
+
+sub2 is assigned to the string that is the reverse of the characters in str1
+
+sub3 is assigned to the string that is comprised of every other character in
+str1
+'''

3838c059292e3ffb1133c2f54f475195a95567d2
vd141 <victor.dinh141@gmail.com>
2024-11-23 18:05:41 -0800

    initial commit

---
+++ Written_Assessment_Prep/Python_SPOT_Wiki/boolean_vs_truthiness.py
@@ -0,0 +1,13 @@
+'''
+In Python, what values are considered Falsy and Truthy?
+Falsy values are
+empty collectibles e.g.: empty lists[], empty tuples (), etc.
+empty strings: ''
+the number 0 represented as a float or integer
+None
+False
+
+Truthy values are any data types that are not those
+
+Truthy/Falsy values are said to evaluate to True/False
+'''

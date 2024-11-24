fe68748918c14591bb75f95ca7f37fa1e8b74e17
vd141 <victor.dinh141@gmail.com>
2024-11-24 13:17:50 -0800

    initial commit and completion of questions

---
+++ Written_Assessment_Prep/Python_SPOT_Wiki/list_and_dictionary_syntax.py
@@ -0,0 +1,15 @@
+'''
+What categories are lists and dictionaries?
+ - Lists and dictionaries are types of collections
+Are they immutable or immutable?
+ - They are both mutable
+Are they primitive or non-primitive?
+ - They are both non-primitive
+Are they literals, or do they require type constructors?
+ - They can be constructed using literals or type constructors
+Are they sequences?
+ - Lists are sequences, dictionaries are not
+Does the order of the elements in both matter?
+ - Lists retain the order of their elements. Dictionaries retain the insertion order
+   of their key-value pairs.
+'''

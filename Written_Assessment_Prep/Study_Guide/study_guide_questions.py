cafdf69d4f2b10852ec09891aa4019d5e214ef69
vd141 <victor.dinh141@gmail.com>
2024-11-26 15:45:40 -0800

    initial commit

---
+++ Written_Assessment_Prep/Study_Guide/study_guide_questions.py
@@ -0,0 +1,11 @@
+'''
+Naming conventions
+
+What is the differene between legal and idiomatic?
+
+What is the difference between illegal vs. non-idiomatic?
+
+What is a legal naming convention?
+
+What is an idiomatic convention?
+'''
